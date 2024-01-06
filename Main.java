e9fb0d360778d73ffbd70564c2fc251198705d44
fhdshk <fhdshk000@gmail.com>
2024-01-07 00:00:21 +0530

    added second line

--- Main.java
+++ Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println(new Date());
         System.out.println("Git update");
         System.out.println("New Line Added");
+        System.out.println("Second Line Added");
     }
 }
